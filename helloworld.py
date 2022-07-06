f5a647a64ae3188222aec4e33c354a95b6da2a5f
Kyle Ashley <89463912+kashley90@users.noreply.github.com>
2022-07-05 21:47:03 -0700

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,14 @@
+# 1. TASK: print "Hello World"
+print( "Hello World!" )
+# 2. print "Hello Noelle!" with the name in a variable
+name = "Kyle!"
+print("Hello",name)	# with a comma
+print("Hello"+name)	# with a +
+# 3. print "Hello 42!" with the number in a variable
+name = 78
+print("Hello",name)	# with a comma
+print("Hello"+ name)
+fave_food1 = "sushi"
+fave_food2 = "pizza"
+print("I love to eat {} and {}.".format(fave_food1,fave_food2))
+print(f"I love to eat {fave_food1} and {fave_food2}")
